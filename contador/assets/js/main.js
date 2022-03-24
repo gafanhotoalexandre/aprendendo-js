9048d98b5fb2cd9c615d8c972a50453b848a0ef4
Alexandre Martins <alexandrevmartinsdelima@gmail.com>
2022-03-24 13:44:41 -0300

    criando timer

--- contador/assets/js/main.js
+++ contador/assets/js/main.js
@@ -1,19 +1,41 @@
+function getZeroHour(seconds) {
+    const date = new Date(seconds * 1000);
+    const hour = date.toLocaleTimeString('pt-br', { hour12: false, timeZone: 'UTC' });
+    
+    return hour;
+}
+
 const watch = document.querySelector('#watch');
 const init = document.querySelector('#init');
 const pause = document.querySelector('#pause');
 const reset = document.querySelector('#reset');
+let seconds = 0;
+let timer;
+
+function startWatch() {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        seconds++;
+        watch.innerHTML = getZeroHour(seconds);
+    }, 1000);
+}
 
 init.addEventListener('click', () => {
     watch.classList.remove('pause');
-    watch.innerHTML = 'iniciando...';
+    
+    startWatch();
 });
 
 pause.addEventListener('click', () => {
     watch.classList.add('pause');
-    watch.innerHTML = 'pausando...';
+
+    clearInterval(timer);
 });
 
 reset.addEventListener('click', () => {
     watch.classList.remove('pause');
-    watch.innerHTML = 'resetando...';
+
+    clearInterval(timer);
+    watch.innerHTML = '00:00:00';
+    seconds = 0;
 });
