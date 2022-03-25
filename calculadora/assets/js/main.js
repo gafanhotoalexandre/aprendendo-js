ecd528dded11496ad1d4e5cb3b36146964e33409
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-03-25 19:29:10 -0300

    finalizando calculadora

--- calculadora/assets/js/main.js
+++ calculadora/assets/js/main.js
@@ -5,9 +5,32 @@ function createCalculator() {
 
         init() {
             this.buttonsClick();
+            this.pressEnter();
+        },
+        pressEnter() {
+            this.display.addEventListener('keyup', event => {
+                if (event.keyCode === 13) {
+                    this.calculate();
+                }
+            });
         },
         calculate() {
-            console.log('calcular');
+            let count = this.display.value;
+
+            try {
+                count = eval(count);
+
+                if (! count) {
+                    alert('Conta inválida!');
+                    return;
+                }
+
+                this.display.value = count;
+            } catch (error) {
+                console.log(count);
+                alert('Conta inválida! (catch)');
+                return;
+            }
         },
         clearDisplay() {
             this.display.value = '';
