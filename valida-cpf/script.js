940f108691b4dbefd78d6253e4da4d629f02a55f
Alexandre Martins <alexandrevmartinsdelima@gmail.com>
2022-03-29 12:18:50 -0300

    finalizando validador

--- valida-cpf/script.js
+++ valida-cpf/script.js
@@ -1,4 +1,4 @@
-
+// CPFs: 705.484.450-52 | 
 function CPFValidate(cpf) {
     Object.defineProperty(this, 'cleanCPF', {
         enumerable: true,
@@ -11,11 +11,15 @@ function CPFValidate(cpf) {
 CPFValidate.prototype.validate = function () {
     if (typeof this.cleanCPF === 'undefined') return false;
     if (this.cleanCPF.length !== 11) return false;
+    if (this.isSequence()) return false;
 
     const parcialCPF = this.cleanCPF.slice(0, -2);
     const firstDigit = this.createDigit(parcialCPF);
+    const secondDigit = this.createDigit(parcialCPF + firstDigit);
 
-    return true;
+    const newCPF = parcialCPF + firstDigit + secondDigit;
+
+    return newCPF === this.cleanCPF;
 };
 CPFValidate.prototype.createDigit = function (parcialCPF) {
     const cpfArray = Array.from(parcialCPF);
@@ -23,14 +27,17 @@ CPFValidate.prototype.createDigit = function (parcialCPF) {
     let regressive = cpfArray.length + 1;
     const total = cpfArray.reduce((accumulator, value) => {
         accumulator += regressive * Number(value);
-        console.log(value, regressive, regressive * value);
         regressive--;
         return accumulator;
     }, 0);
 
     const digit = 11 - (total % 11);
 
-    console.log(digit);
+    return digit > 9 ? '0' : String(digit);
+};
+CPFValidate.prototype.isSequence = function () {
+    const sequence = this.cleanCPF[0].repeat(this.cleanCPF.length);
+    return sequence === this.cleanCPF;
 };
 
 
