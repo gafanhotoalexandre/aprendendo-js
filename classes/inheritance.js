3cc275b15568334af9c7f20c97c1079cb5960e23
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-03-31 20:25:52 -0300

    classes: trabalhando heranca

--- classes/inheritance.js
+++ classes/inheritance.js
@@ -13,5 +13,16 @@ class User {
 }
 
 class Student extends User {
-    
+    constructor(name, course) {
+        super(name);
+        this.course = course;
+    }
+
+    salutation() {
+        return `Olá, eu me chamo ${this.name} e estou no curso de ${this.course}`;
+    }
 }
+
+const ale = new Student('Alexandre', 'ADS');
+ale.goOnline();
+console.log(ale.online);
