4cae18dae4f891105a102295b7b6cc024726d938
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-04-03 11:16:45 -0300

    Promise.resolve e Promise.reject

--- js-assincrono/metodos.promise.js
+++ js-assincrono/metodos.promise.js
@@ -17,6 +17,7 @@ function waitForIt(message, time) {
 }
 
 // Promise.all() | Promise.race() | Promise.resolve() | Promise.reject()
+/*
 const promises = [
     // 'Primeiro valor',
     waitForIt('Primeira Promise', rand(1, 3)),
@@ -28,4 +29,19 @@ const promises = [
 
 Promise.race(promises)
     .then(value => console.log(value))
+    .catch(error => console.log('Erro:', error));
+*/
+
+function downloadPage() {
+    const inCache = true;
+
+    if (inCache) {
+        return Promise.reject('Apresentando página em cache');
+    } else {
+        return waitForIt('Baixei a página e a apresentei', 3000);
+    }
+}
+
+downloadPage()
+    .then(data => console.log(data))
     .catch(error => console.log('Erro:', error));
