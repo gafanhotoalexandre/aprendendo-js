19d61d02ccfca3077b00d9710501e4aa8220916b
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-04-04 23:45:34 -0300

    testando babel via npm

---
+++ babel/bundle.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.user = void 0;
+
+var user = function user(name) {
+  return {
+    name: name
+  };
+};
+
+exports.user = user;
