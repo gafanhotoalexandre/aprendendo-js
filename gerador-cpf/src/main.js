0420174deb7f2c9a1e3c5afe23ee84645cb8aa26
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-04-07 23:34:48 -0300

    retornando cpf valido para view

--- gerador-cpf/src/main.js
+++ gerador-cpf/src/main.js
@@ -1,3 +1,11 @@
 import CPFGenerator from './modules/CPFGenerator';
 
 import './assets/css/style.css';
+
+(function() {
+    const generator = new CPFGenerator();
+    const cpf = generator.generateNewCPF();
+
+    const result = document.querySelector('#result');
+    result.innerText = cpf;
+})();
--- gerador-cpf/src/modules/CPFGenerator.js
+++ gerador-cpf/src/modules/CPFGenerator.js
@@ -1,3 +1,16 @@
 import CPFValidator from './CPFValidator';
 
+export default class CPFGenerator {
+    rand(min = 100000000, max = 999999999) {
+        return String(Math.floor(Math.random() * (max - min) + min));
+    }
 
+    generateNewCPF() {
+        const partialCPF = this.rand();
+        const firstDigit = CPFValidator.digitGenerate(partialCPF);
+        const secondDigit = CPFValidator.digitGenerate(partialCPF + firstDigit);
+
+        const newCPF = partialCPF + firstDigit + secondDigit;
+        return newCPF;
+    }
+}
