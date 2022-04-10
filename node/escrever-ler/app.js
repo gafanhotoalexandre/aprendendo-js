9b56d5ae65b0ecb7ddd6bf953e1d3e403b7331b6
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-04-09 23:27:58 -0300

    lendo arquivos

--- node/escrever-ler/app.js
+++ node/escrever-ler/app.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const filePath = path.resolve(__dirname, 'test.json');
-const writer = require('./modules/Write');
 
+const writer = require('./modules/Write');
+const reader = require('./modules/Read');
+
+/*
 const people = [
     { name: 'Alexandre' },
     { name: 'Verônica' },
@@ -11,3 +14,17 @@ const people = [
 
 const json = JSON.stringify(people, '', 2);
 writer(filePath, json);
+*/
+
+async function readFile(filePath) {
+    const data = await reader(filePath);
+    dataRender(data);
+}
+
+function dataRender(data) {
+    data = JSON.parse(data);
+    data.forEach(person => console.log(person.name));
+}
+
+readFile(filePath);
+
