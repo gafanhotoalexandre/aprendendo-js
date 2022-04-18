a627c6928641ed666659707c5a6ad89ad4d52b76
Alexandre Martins <alexandrevmartinsdelima@gmail.com>
2022-04-18 10:07:55 -0300

    iniciando projeto

---
+++ agenda/server.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const connectionString = process.env.CONNECTION_STRING;
+
+const port = 3000;
+
+mongoose.connect(connectionString)
+    .then(() => {
+        app.emit('ok');
+    })
+    .catch(error => console.log(error));
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+const helmet = require('helmet');
+const csrf = require('csurf');
+
+const routes = require('./routes');
+const { globalMiddleware, checkCSRFError, csrfMiddleware } = require('./src/middlewares/globalMiddleware');
+
+// comentando utilização do helmet para evitar erros em ambiente localhost (de desenvolvimento)
+// app.use(helmet());
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+// configurando sessão
+const sessionOptions = session({
+    secret: 'lldfsdklfkdlodfdkflfwslkdlksa aldksaldksldkslskadçd',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTION_STRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+});
+app.use(sessionOptions);
+// configurando flash messages
+app.use(flash());
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(csrf());
+// meus middlewares
+app.use(globalMiddleware);
+app.use(checkCSRFError);
+app.use(csrfMiddleware);
+app.use(routes);
+
+app.on('ok', () => {
+    app.listen(port, () => {
+        console.log('Acessar http://localhost:' + port);
+    });
+});
+
