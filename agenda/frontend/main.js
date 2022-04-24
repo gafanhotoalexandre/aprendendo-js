c4b04a0eb3e56a044bc9dc8c3f77ec3ebaf46c48
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-04-23 21:14:38 -0300

    trabalhando validacao no frontend

--- agenda/frontend/main.js
+++ agenda/frontend/main.js
@@ -1,4 +1,11 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime'
 
-import './assets/css/style.css';
+import Login from './modules/Login';
+
+const register = new Login('.register-form');
+const login = new Login('.login-form');
+register.init();
+login.init();
+
+// import './assets/css/style.css';
