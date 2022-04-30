baea02a4522ebfa094936dc1c8e3f22a06d1045e
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-04-30 19:56:13 -0300

    criando e aplicando middleware loginRequired

--- api_rest/src/routes/userRoutes.js
+++ api_rest/src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = Router();
 
-router.get('/', userController.index);
+router.get('/', loginRequired, userController.index);
 router.get('/:id', userController.show);
 router.post('/', userController.store);
 router.put('/:id', userController.update);
