e9c406a44be734136a3e0c61bd28cb4cab6625c6
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-02 07:58:42 -0300

    criando rotas para upload de imagens

---
+++ api_rest/src/controllers/PhotoController.js
@@ -0,0 +1,7 @@
+class PhotoController {
+  async store(req, res) {
+    res.json('PHOTO');
+  }
+}
+
+export default new PhotoController();
---
+++ api_rest/src/routes/photoRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import photoController from '../controllers/PhotoController';
+
+const router = Router();
+
+router.post('/', photoController.store);
+
+export default router;
