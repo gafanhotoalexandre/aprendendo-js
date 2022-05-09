a9856733b0d51f57e6658a855951f32aab06c96d
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-09 16:17:38 -0300

    validando mimetype de arquivo pelo multer

--- api_rest/src/config/multer.js
+++ api_rest/src/config/multer.js
@@ -4,6 +4,13 @@ import { extname, resolve } from 'path';
 const random = () => Math.floor(Math.random() * (20000 - 10000) + 10000);
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('Formato do arquivo precisa ser PGN ou JPG'));
+    }
+
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
