ed15b6adb8efeeb26241ac77cbe88c702477e523
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-09 16:24:37 -0300

    retornando feedback de erro

--- api_rest/src/controllers/PhotoController.js
+++ api_rest/src/controllers/PhotoController.js
@@ -1,6 +1,19 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('photo');
+
 class PhotoController {
   async store(req, res) {
-    res.json('PHOTO');
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
