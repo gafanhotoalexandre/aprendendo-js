b9ef57ca5bf1e6a474ab89772b0c27dd8fdfff14
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-09 17:28:16 -0300

    criando model e migration para photos

---
+++ api_rest/src/models/Photo.js
@@ -0,0 +1,30 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Photo extends Model {
+  static init(sequelize) {
+    super.init({
+      originalname: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'O campo não pode ficar vazio.',
+          },
+        },
+      },
+      filename: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'O campo não pode ficar vazio.',
+          },
+        },
+      },
+    }, {
+      sequelize,
+      tableName: 'photos',
+    });
+    return this;
+  }
+}
--- api_rest/src/routes/photoRoutes.js
+++ api_rest/src/routes/photoRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import loginRequired from '../middlewares/loginRequired';
+
 import photoController from '../controllers/PhotoController';
 
 const router = Router();
 
-router.post('/', photoController.store);
+router.post('/', loginRequired, photoController.store);
 
 export default router;
