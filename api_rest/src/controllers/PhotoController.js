b884235325d9eeb21b67a4c484a3bf80098491f9
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-09 22:56:33 -0300

    salvando foto na base de dados

--- api_rest/src/controllers/PhotoController.js
+++ api_rest/src/controllers/PhotoController.js
@@ -1,18 +1,24 @@
 import multer from 'multer';
 import multerConfig from '../config/multer';
 
+import Photo from '../models/Photo';
+
 const upload = multer(multerConfig).single('photo');
 
 class PhotoController {
-  async store(req, res) {
-    return upload(req, res, (err) => {
+  store(req, res) {
+    return upload(req, res, async (err) => {
       if (err) {
         return res.status(400).json({
           errors: [err.code],
         });
       }
 
-      return res.json(req.file);
+      const { originalname, filename } = req.file;
+      const { aluno_id } = req.body;
+      const photo = await Photo.create({ originalname, filename, aluno_id });
+
+      return res.json(photo);
     });
   }
 }
