1fc4418cbb56cebe80bc50d0a32a9d1f4dd984d6
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-10 22:44:37 -0300

    definindo regras do eslint

---
+++ react/lista-tarefas/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
