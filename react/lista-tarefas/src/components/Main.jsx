5b04dd8af03bc1572f43e03a4f92f2835ca7096a
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-12 19:32:06 -0300

    estilizando o form e utilizando react-icons

--- react/lista-tarefas/src/components/Main.jsx
+++ react/lista-tarefas/src/components/Main.jsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
+
+// Form
+import { FaPlus } from 'react-icons/fa';
+
 import './Main.css';
 
 export function Main() {
+  const [task, setTask] = useState('');
+
   return (
     <div className="main">
       <h1>Lista de Tarefas</h1>
 
-      <form action="#">
-        <input type="text" />
+      <form action="#" className="form">
+        <input
+          type="text"
+          onChange={(e) => setTask(e.target.value)}
+          value={task}
+        />
 
-        <button type="button">Enviar</button>
+        <button type="submit">
+          <FaPlus />
+        </button>
       </form>
     </div>
   );
