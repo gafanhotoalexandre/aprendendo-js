407b6c13b98c15c9d016293e6b39b1c07c47d293
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-12 19:11:50 -0300

    iniciando componentes

---
+++ react/lista-tarefas/src/components/Main.jsx
@@ -0,0 +1,15 @@
+import './Main.css';
+
+export function Main() {
+  return (
+    <div className="main">
+      <h1>Lista de Tarefas</h1>
+
+      <form action="#">
+        <input type="text" />
+
+        <button type="button">Enviar</button>
+      </form>
+    </div>
+  );
+}
