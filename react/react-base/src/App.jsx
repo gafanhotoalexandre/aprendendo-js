5297ed80a0baa3e73942520fb78c0d834e1b3614
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-13 22:09:04 -0300

    criando estilo global

--- react/react-base/src/App.jsx
+++ react/react-base/src/App.jsx
@@ -1,10 +1,13 @@
 import { Login } from './pages/Login';
+import GlobalStyles, { Container } from './styles/GlobalStyles';
 
 export function App() {
   return (
-    <>
+    <Container>
       <h1>Hello World</h1>
       <Login />
-    </>
+      <GlobalStyles />
+      <button type="button">Clique aqui</button>
+    </Container>
   );
 }
---
+++ react/react-base/src/styles/GlobalStyles.js
@@ -0,0 +1,40 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: #eee;
+  }
+
+  html, body, #root {
+    height: 100vh;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+`;
+
+export const Container = styled.section`
+  max-width: 360px;
+  background-color: #fff;
+  padding: 30px;
+  margin: 30px auto;
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
